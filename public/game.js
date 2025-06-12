00c7d3fa257f0f84632312eb9247132043b5a2df
AKARuberDuck <ss2875356@gmail.com>
2025-06-12 13:33:09 -0500

    Update game.js

--- public/game.js
+++ public/game.js
@@ -13,7 +13,6 @@ window.addEventListener("DOMContentLoaded", function () {
     scene = new BABYLON.Scene(engine);
     scene.enablePhysics();
     scene.gravity = new BABYLON.Vector3(0, -0.5, 0);
-const music = new BABYLON.Sound(…);
     const camera = new BABYLON.ArcRotateCamera("camera", Math.PI / 2.5, Math.PI / 2.7, 30, BABYLON.Vector3.Zero(), scene);
     camera.attachControl(canvas, true);
     new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
